15b414c57490c1f903ed0427a7a655784045b57c
Anderson Schmitt <a.schmitt@ibracem.org.br>
2022-02-22 12:58:46 -0300

    Code updated. Many tries to solve Font problem. None of them worked.

--- Web.Api.Teste/Controllers/IndexController.cs
+++ Web.Api.Teste/Controllers/IndexController.cs
@@ -1,8 +1,12 @@
-﻿using NReco.PdfGenerator;
+﻿using System;
+using System.IO;
+using NReco.PdfGenerator;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Web.Hosting;
 using System.Web.Http;
+using Web.Api.Teste.Helpers;
 
 namespace Web.Api.Teste.Controllers
 {
@@ -11,23 +15,38 @@ namespace Web.Api.Teste.Controllers
         // GET api/values
         public IHttpActionResult Get()
         {
-            string html = "<html><head><style>" +
-                          "body, td { font: 16px Arial }" +
-                          ".tab1 { border: 1px #CCCCCC solid; border-radius: 15px; padding: 10px }" +
-                          ".tab1 td { padding: 5px 10px }" +
-                          ".td1e { background-color: #EEEEEE; width: 100px; text-align: right; padding: 5px 10px }" +
-                          ".tab2 { border: 1px #CCCCCC solid; border-radius: 15px; padding: 10px; width: 100%; background-color: #EEEEEE }" +
-                          ".tab2 td { text-align: center; font-weight: bold } " +
-                          "</style></head><body>" +
-                          "<b style=\"color: #5466dd; font-size: 22px\">REPRESENTAÇÕES<br />ENCAMINHADAS</b><br /><br />" +
-                          "<table class=\"tab1\">" +
-                          "<tr><td class=\"td1e\">CNPJ base</td><td>00.111.222/3333-44</td></tr>" +
-                          "</table><br /><br />" +
-                          "<table class=\"tab2\"><tr><td>Nenhum registro foi encontrado.</td></tr></table><br /><br />" +
-                          "Data da consulta: 08/01/1987 às 00:50.<br />" +
-                          "<b>Fonte:</b> Base Interna, período de 01/2020, atualizada em 12/02/2022.<br />" +
-                          "Portaria AFB nº 1.750 de 12/04/2014, artigo 16." +
-                          "</body></html>";
+            string html = $@"
+                            <html>
+                                <head>
+                                    <style>
+                                        body, td {{ font: 16px ""Arial"" }}
+                                        .tab1 {{ border: 1px #CCCCCC solid; border-radius: 15px; padding: 10px }}
+                                        .tab1 td {{ padding: 5px 10px }}
+                                        .td1e {{ background-color: #EEEEEE; width: 100px; text-align: right; padding: 5px 10px }}
+                                        .tab2 {{ border: 1px #CCCCCC solid; border-radius: 15px; padding: 10px; width: 100%; background-color: #EEEEEE }}
+                                        .tab2 td {{ text-align: center; font-weight: bold }}
+                                    </style>
+                                </head>
+                                <body>
+                                    <b style=""color: #5466dd; font-size: 22px"">
+                                        REPRESENTAÇÕES<br />ENCAMINHADAS
+                                    </b><br /><br />
+                                    <table class=""tab1"">
+                                        <tr>
+                                            <td class=""td1e"">CNPJ base</td>
+                                            <td>00.111.222/3333-44</td>
+                                        </tr>
+                                    </table><br /><br />
+                                    <table class=""tab2"">
+                                        <tr>
+                                            <td>Nenhum registro foi encontrado.</td>
+                                        </tr>
+                                    </table><br /><br />
+                                    Data da consulta: 08/01/1987 às 00:50.<br />
+                                    <b>Fonte:</b> Base Interna, período de 01/2020, atualizada em 12/02/2022.<br />
+                                    Portaria AFB nº 1.750 de 12/04/2014, artigo 16.
+                                </body>
+                            </html>";
 
             HtmlToPdfConverter pdf = new HtmlToPdfConverter();
             pdf.CustomWkHtmlArgs = "--print-media-type --encoding utf-8";
--- Web.Api.Teste/Global.asax.cs
+++ Web.Api.Teste/Global.asax.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Diagnostics;
 using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
 
 namespace Web.Api.Teste
 {
